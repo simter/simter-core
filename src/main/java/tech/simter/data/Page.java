f421ebf8ccd935f3331ea6136726ac95582a777a
RJ.Hwang <rongjihuang@gmail.com>
2017-04-27 19:35:59 +0800

    Add Page interface and builder method

---
+++ src/main/java/tech/simter/data/Page.java
@@ -0,0 +1,177 @@
+/**
+ *
+ */
+package tech.simter.data;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A page is a sublist of a list of objects.
+ * It allows gain information about the position of it in the containing entire list.
+ * <p>
+ * The main interface methods just 4 of below, all other method can be calculate from them:
+ * <ul>
+ * <li>int getNumber() - The page number, it's one of the page conditions</li>
+ * <li>int getCapacity() - The page capacity, it's the maximal size of each page, it's one of the page conditions</li>
+ * <li>int getRows() - All real row data of this page, it's one of the page results</li>
+ * <li>int getTotalCount() - The total amount of all data store in the repository, it's one of the page results</li>
+ * </ul>
+ *
+ * @param <T> the type of the row data
+ * @author RJ 2017-04-27
+ */
+public interface Page<T> extends Serializable {
+  /**
+   * The number of this page. It's minimal value is 1.
+   *
+   * @return the number of this page
+   */
+  int getNumber();
+
+  /**
+   * The maximal size of each page. It's minimal value is 1.
+   *
+   * @return the maximal size of each page
+   */
+  int getCapacity();
+
+  /**
+   * All the row data of this page.
+   * Returns {@link Collections#emptyList()} if this page has no data. Implementer should never return null.
+   *
+   * @return all the real row data of this page
+   */
+  List<T> getRows();
+
+  /**
+   * The total amount of all rows in the store.
+   *
+   * @return zero if the repository has no data
+   */
+  long getTotalCount();
+
+  /**
+   * The number of total pages. It's minimal value is 0.
+   *
+   * @return zero if the repository has no data
+   */
+  int getPageCount();
+
+  /**
+   * The offset to be taken according to the page number and capacity. It's minimal value is 0.
+   *
+   * @return The offset to be taken according to the page number and capacity
+   */
+  int getOffset();
+
+  /**
+   * Whether the page has data at all.
+   *
+   * @return true if this page has data, otherwise return false
+   */
+  boolean isEmpty();
+
+  /**
+   * The real data size of this page. It's minimal value is 0.
+   *
+   * @return the real data size of this page
+   */
+  int getSize();
+
+  /**
+   * Whether the current page is the first page.
+   *
+   * @return true if this page is the first page, otherwise return false
+   */
+  boolean isFirst();
+
+  /**
+   * Whether the current page is the last page.
+   *
+   * @return true if this page is the last page, otherwise return false
+   */
+  boolean isLast();
+
+  /**
+   * Calculate the offset value by pageNo and capacity
+   *
+   * @param pageNo  the page number
+   * @param maxSize the page maximal size
+   * @return the offset value
+   */
+  static int calculateOffset(int pageNo, int maxSize) {
+    pageNo = pageNo < 1 ? 1 : pageNo;
+    maxSize = maxSize < 1 ? 1 : maxSize;
+    return (pageNo - 1) * maxSize;
+  }
+
+  /**
+   * Build a new page instance.
+   *
+   * @param number     the page number
+   * @param capacity   the page maximal size
+   * @param rows       the page data
+   * @param totalCount the total amount of all rows in the store
+   * @param <T>        the type of the row data
+   * @return the page instance holds the data
+   */
+  static <T> Page<T> build(int number, int capacity, List<T> rows, long totalCount) {
+    int number_ = Math.max(1, number);
+    int capacity_ = Math.max(1, capacity);
+    List<T> rows_ = rows != null ? Collections.unmodifiableList(rows) : Collections.emptyList();
+    long totalCount_ = Math.max(0, totalCount);
+    return new Page<T>() {
+      @Override
+      public int getNumber() {
+        return number_;
+      }
+
+      @Override
+      public int getCapacity() {
+        return capacity_;
+      }
+
+      @Override
+      public List<T> getRows() {
+        return rows_;
+      }
+
+      @Override
+      public long getTotalCount() {
+        return totalCount_;
+      }
+
+      @Override
+      public int getPageCount() {
+        return (int) Math.ceil((double) totalCount_ / (double) capacity_);
+      }
+
+      @Override
+      public int getOffset() {
+        return (number_ - 1) * capacity_;
+      }
+
+      @Override
+      public boolean isEmpty() {
+        return rows_.isEmpty();
+      }
+
+      @Override
+      public int getSize() {
+        return rows_.size();
+      }
+
+      @Override
+      public boolean isFirst() {
+        return number_ <= 1;
+      }
+
+      @Override
+      public boolean isLast() {
+        return number_ + 1 > getPageCount();
+      }
+    };
+  }
+}
---
+++ src/test/java/tech/simter/data/PageTest.java
@@ -0,0 +1,107 @@
+package tech.simter.data;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author RJ 2017-04-27
+ */
+public class PageTest {
+  @Test
+  public void empty() throws Exception {
+    Page<Object> page = Page.build(0, 0, null, 0);
+    assertThat(page.getNumber(), is(1));
+    assertThat(page.getCapacity(), is(1));
+    assertThat(page.getRows(), notNullValue());
+    assertThat(page.getTotalCount(), is(0L));
+
+    assertThat(page.getPageCount(), is(0));
+    assertThat(page.getSize(), is(0));
+    assertThat(page.getOffset(), is(0));
+    assertThat(page.isEmpty(), is(true));
+    assertThat(page.isFirst(), is(true));
+    assertThat(page.isLast(), is(true));
+  }
+
+  @Test
+  public void firstPage() throws Exception {
+    List<String> items = new ArrayList<>();
+    items.add("1");
+    Page<String> page = Page.build(1, 25, items, 101);
+    assertThat(page.getNumber(), is(1));
+    assertThat(page.getCapacity(), is(25));
+    assertThat(page.getRows(), notNullValue());
+    assertThat(page.getTotalCount(), is(101L));
+
+    assertThat(page.getPageCount(), is(5));
+    assertThat(page.getSize(), is(1));
+    assertThat(page.getOffset(), is(0));
+    assertThat(page.isEmpty(), is(false));
+    assertThat(page.isFirst(), is(true));
+    assertThat(page.isLast(), is(false));
+  }
+
+  @Test
+  public void secondPage() throws Exception {
+    List<String> items = new ArrayList<>();
+    items.add("1");
+    Page<String> page = Page.build(2, 25, items, 101);
+    assertThat(page.getNumber(), is(2));
+    assertThat(page.getCapacity(), is(25));
+    assertThat(page.getRows(), notNullValue());
+    assertThat(page.getTotalCount(), is(101L));
+
+    assertThat(page.getPageCount(), is(5));
+    assertThat(page.getSize(), is(1));
+    assertThat(page.getOffset(), is(25));
+    assertThat(page.isEmpty(), is(false));
+    assertThat(page.isFirst(), is(false));
+    assertThat(page.isLast(), is(false));
+
+    // another
+    page = Page.build(3, 25, items, 101);
+    assertThat(page.getNumber(), is(3));
+    assertThat(page.getCapacity(), is(25));
+    assertThat(page.getRows(), notNullValue());
+    assertThat(page.getTotalCount(), is(101L));
+
+    assertThat(page.getPageCount(), is(5));
+    assertThat(page.getSize(), is(1));
+    assertThat(page.getOffset(), is(50));
+    assertThat(page.isEmpty(), is(false));
+    assertThat(page.isFirst(), is(false));
+    assertThat(page.isLast(), is(false));
+  }
+
+  @Test
+  public void lastPage() throws Exception {
+    List<String> items = new ArrayList<>();
+    items.add("1");
+    Page<String> page = Page.build(5, 25, items, 101);
+    assertThat(page.getNumber(), is(5));
+    assertThat(page.getCapacity(), is(25));
+    assertThat(page.getRows(), notNullValue());
+    assertThat(page.getTotalCount(), is(101L));
+
+    assertThat(page.getPageCount(), is(5));
+    assertThat(page.getSize(), is(1));
+    assertThat(page.getOffset(), is(100));
+    assertThat(page.isEmpty(), is(false));
+    assertThat(page.isFirst(), is(false));
+    assertThat(page.isLast(), is(true));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void canNotModifyPageData() throws Exception {
+    List<String> items = new ArrayList<>();
+    items.add("1");
+    Page<String> page = Page.build(1, 25, items, 101);
+    page.getRows().add("something");
+  }
+}
